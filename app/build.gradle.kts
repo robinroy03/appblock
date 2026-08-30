8b8987479aa57ef706c35e951bc9f0d7da36422b
robinroy03 <robinroy.work@gmail.com>
2026-08-30 23:46:02 +0530

    Bump version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.robin.appblock"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 
     if (!keystoreProps.isEmpty) {
