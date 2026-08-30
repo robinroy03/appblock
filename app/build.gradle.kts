5e4b77eb096f45f36c6f7b74d2219b66d1235eda
robinroy03 <robinroy.work@gmail.com>
2026-08-31 01:03:44 +0530

    Bump version to 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.robin.appblock"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 
     if (!keystoreProps.isEmpty) {
